b85f9962f8caf3da79e7ff4b3de4d38e12e30e18
South-River <278042969@qq.com>
2022-03-23 23:10:08 +0800

    update code

--- src/main/src/main.cpp
+++ src/main/src/main.cpp
@@ -1,10 +1,51 @@
 #include <iostream>
 #include <ros/ros.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
 #include <std_msgs/Bool.h>
 #include <geometry_msgs/Pose.h>
 
 bool target_achieved = false;
 bool chessboard_detected = false;
+
+geometry_msgs::Pose cam2robot;
+cv::Mat T = cv::Mat::eye(cv::Size(4,4), CV_64F);
+
+void fromQuaternion2RPY(const geometry_msgs::Pose& pose, double& roll, double& pitch, double& yaw)
+{
+        double w = pose.orientation.w;
+        double x = pose.orientation.x;
+        double y = pose.orientation.y;
+        double z = pose.orientation.z;
+
+        roll = atan2(2*(w*x+y*z), pow(w,2) - pow(x,2) - pow(y,2) + pow(z,2));
+        pitch = asin(2*(w*y-x*z));
+        yaw = atan2(2*(w*z+x*y), pow(w,2) + pow(x,2) - pow(y,2) - pow(z,2));
+}
+
+void fromRPY2R(const double& roll, const double& pitch, const double& yaw, cv::Mat& R)
+{
+        cv::Mat Rx = cv::Mat::eye(cv::Size(3,3), CV_64FC1);
+        cv::Mat Ry = cv::Mat::eye(cv::Size(3,3), CV_64FC1);
+        cv::Mat Rz = cv::Mat::eye(cv::Size(3,3), CV_64FC1);
+
+        Rx.at<double>(1,1) = std::cos(roll);
+        Rx.at<double>(1,2) = -std::sin(roll);
+        Rx.at<double>(2,1) = std::sin(roll);
+        Rx.at<double>(2,2) = std::cos(roll);
+
+        Ry.at<double>(0,0) = std::cos(pitch);
+        Ry.at<double>(0,2) = std::sin(pitch);
+        Ry.at<double>(2,0) = -std::sin(pitch);
+        Ry.at<double>(2,2) = std::cos(pitch);
+
+        Rz.at<double>(0,0) = std::cos(yaw);
+        Rz.at<double>(0,1) = -std::sin(yaw);
+        Rz.at<double>(1,0) = std::sin(yaw);
+        Rz.at<double>(1,1) = std::cos(yaw);
+
+        R = Rz*Ry*Rx; 
+}
 
 void targetAchieveCallBack(const std_msgs::Bool::ConstPtr& msg)
 {
@@ -22,6 +63,33 @@ void chessboardDetectedCallBack(const std_msgs::Bool::ConstPtr& msg)
         }
 }
 
+void robotPoseCallBack(const geometry_msgs::Pose::ConstPtr& msg)
+{
+        cam2robot = *msg;
+        
+        ros::NodeHandle nh;
+        ros::Publisher go_home_pub = nh.advertise<std_msgs::Bool>("/robot/go_home", 10);
+
+        std_msgs::Bool go_home_msg;
+        go_home_msg.data = true;
+        go_home_pub.publish(go_home_msg);
+
+        double  roll, pitch, yaw;
+        fromQuaternion2RPY(cam2robot, roll, pitch, yaw);
+        
+        cv::Mat R = cv::Mat::eye(cv::Size(3,3), CV_64FC1);
+        fromRPY2R(roll, pitch, yaw, R);
+        
+        for(int i = 0; i < 9; i ++)
+        {
+                T.at<double>(i/3, i%3) = R.at<double>(i/3, i%3);
+        }
+
+        T.at<double>(0,3) = cam2robot.position.x;
+        T.at<double>(1,3) = cam2robot.position.y;
+        T.at<double>(2,3) = cam2robot.position.z;
+}
+
 int main(int argc, char** argv)
 {
         ros::init(argc, argv, "main");
@@ -32,17 +100,18 @@ int main(int argc, char** argv)
 
         ros::Subscriber target_achieve_sub = nh.subscribe("/robot/pose_achieved", 10, targetAchieveCallBack);
         ros::Subscriber chessboard_detect_sub = nh.subscribe("/vision/chessboard/had_detected", 10, chessboardDetectedCallBack);
+        ros::Subscriber robot_pose_sub = nh.subscribe("/vision/robot_pose", 10, robotPoseCallBack);
 
         ros::Rate rate(20);
 
         std::vector<geometry_msgs::Pose> target_pose_list;
-        for(int i = 0; i<9; i ++)
+        for(int i = 0; i<27; i ++)
         {
                 geometry_msgs::Pose target_pose;
 
-                target_pose.position.x = -0.1 + 0.1*(i/3);
+                target_pose.position.x = -0.1 + 0.1*((i%9)/3);
                 target_pose.position.y = 0.3 - 0.1*(i%3);
-                target_pose.position.z = 0.35;
+                target_pose.position.z = 0.25 + 0.05*(i/9);
 
                 target_pose.orientation.w = 1.;
 
@@ -63,7 +132,7 @@ int main(int argc, char** argv)
                         rate.sleep();
                 }
                 target_achieved = false;
-                //ROS_INFO("Target Achieved! Coordinate: [ %s, %s, %s]", target_pose_list[i].position.x, target_pose_list[i].position.y, target_pose_list[i].position.z);
+                ROS_INFO("Target Achieved! Coordinate: [ %lf, %lf, %lf]", target_pose_list[i].position.x, target_pose_list[i].position.y, target_pose_list[i].position.z);
 
                 std_msgs::Bool msg;
                 msg.data = true;
@@ -78,5 +147,7 @@ int main(int argc, char** argv)
                 }
                 chessboard_detected = false;
         }
+
+        ros::spin();
         return 0;
 }
--- src/vision/src/vision.cpp
+++ src/vision/src/vision.cpp
@@ -5,11 +5,11 @@
 #include <sensor_msgs/Image.h>
 #include <std_msgs/Bool.h>
 #include <cv_bridge/cv_bridge.h>
-#include <librealsense2/rs.hpp>
+#include <geometry_msgs/Pose.h>
 
 #define m2mm (1000)
 #define rad2ang (180./M_PI)
-#define _CALIBRATE_POINT_NUM  (9)
+#define _CALIBRATE_POINT_NUM  (27)
 
 cv::Mat rgb;
 cv::Mat depth;
@@ -29,7 +29,7 @@ void DepthCallBack(const sensor_msgs::Image::ConstPtr& msg)
 {
         cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(*msg, sensor_msgs::image_encodings::TYPE_16UC1);
         depth = cv_ptr->image;
-        //std::cout << "DEPTH RECEIVED! Size:" <<  depth.size << std::endl;
+        //std::cout << "DEPTH Received! Size:" <<  depth.size << std::endl;
 }
 
 void startDetectCallBack(const std_msgs::Bool::ConstPtr& msg)
@@ -49,7 +49,7 @@ void getCornerList(const cv::Mat& rgb, std::vector<cv::Point2f>& corners, bool& 
                 return;
         }
 
-        cv::imshow("chessboard", rgb);
+        //cv::imshow("chessboard", rgb);
 
         cv::Mat gray;
         cv::cvtColor(rgb, gray, cv::COLOR_BGR2GRAY);
@@ -69,7 +69,7 @@ void getCornerList(const cv::Mat& rgb, std::vector<cv::Point2f>& corners, bool& 
 
         cv::drawChessboardCorners(gray, patternSize, corners, chessboardFound);
         
-        cv::imshow("chessboard", gray);
+        //cv::imshow("chessboard", gray);
         
         chessboard_detected = true;
         return;
@@ -105,8 +105,9 @@ void getCenter(const std::vector<cv::Point2f>& corners, const cv::Mat& cameraMat
 
         cv::Point2f center_pixel = corners.at(corners.size()/2);
         
-        center.z =  (float)depth.at<uint16_t>(center_pixel.x, center_pixel.y);
-        // center.z =  (float)depth.at<uint16_t>(320, 240);
+        center.z =  depth.at<uint16_t>(center_pixel.y, center_pixel.x);
+        //center.z =  (float)depth.at<uint16_t>(320, 240);
+
         center.x = (center_pixel.x - cameraMatrix.at<double>(0,2))*(center.z/cameraMatrix.at<double>(0,0));  
         center.y = (center_pixel.y - cameraMatrix.at<double>(1,2))*(center.z/cameraMatrix.at<double>(1,1));
 }
@@ -114,10 +115,10 @@ void getCenter(const std::vector<cv::Point2f>& corners, const cv::Mat& cameraMat
 void getCameraInfo(cv::Mat& cameraMatrix, cv::Mat& distCoeffs)
 {
         cameraMatrix = cv::Mat::eye(3,3,CV_64F);
-        cameraMatrix.at<double>(0,0) = 609.7652587890625;
-        cameraMatrix.at<double>(0,2) =  319.5198974609375;
-        cameraMatrix.at<double>(1,1) =  609.3673706054688;
-        cameraMatrix.at<double>(1,2) = 242.18905639648438;
+        cameraMatrix.at<double>(0,0) = 914.6478881835938;
+        cameraMatrix.at<double>(0,2) =  639.2798461914062;
+        cameraMatrix.at<double>(1,1) =  914.0510864257812;
+        cameraMatrix.at<double>(1,2) = 363.2835693359375;
 
         std::vector<double> distCoeffs_vec{6.8641294614044765e-02, 
                                                                                 3.2902097109600531e-01,
@@ -130,8 +131,31 @@ void getCameraInfo(cv::Mat& cameraMatrix, cv::Mat& distCoeffs)
 void fromR2rpy(const cv::Mat& R, double& roll, double& pitch, double& yaw)
 {
         roll = atan2(R.at<double>(2,1), R.at<double>(2,2));
-        pitch = atan2(-R.at<double>(2,0), sqrt(pow(R.at<double>(0,0),2)+pow(R.at<double>(1,0),2)));
+        pitch = atan2(-R.at<double>(2,0), sqrt(pow(R.at<double>(2,1),2)+pow(R.at<double>(2,2),2)));
         yaw = atan2(R.at<double>(1,0),R.at<double>(0,0));
+}
+
+void fromrpy2Quaternion(const double& roll, const double& pitch, const double&yaw, geometry_msgs::Pose& pose)
+{
+        cv::Mat mat1(4, 1, CV_64FC1);
+        cv::Mat mat2(4, 1, CV_64FC1);
+        cv::Mat mat3(4, 1, CV_64FC1);
+
+        mat1.at<double>(0,0)=std::cos(yaw/2);
+        mat1.at<double>(3,0)=std::sin(yaw/2);
+
+        mat2.at<double>(0,0)=std::cos(pitch/2);
+        mat2.at<double>(2,0)=std::sin(pitch/2);
+
+        mat3.at<double>(0,0)=std::cos(roll/2);
+        mat3.at<double>(1,0)=std::sin(roll/2);
+
+        cv::Mat q = mat1*mat2*mat3;
+
+        pose.orientation.w=q.at<double>(0,0);
+        pose.orientation.x=q.at<double>(1,0);
+        pose.orientation.y=q.at<double>(2,0);
+        pose.orientation.z=q.at<double>(3,0);
 }
 
 bool ICP(const std::vector<cv::Point3d>& src_points, const std::vector<cv::Point3d>& dst_points, cv::Mat& T, cv::Mat& R, cv::Mat& t)
@@ -180,12 +204,17 @@ bool ICP(const std::vector<cv::Point3d>& src_points, const std::vector<cv::Point
                 dst_decentroid_coordinate.at<double>(2,i) = dst_points[i].z - dst_center.z;
         }
 
-        cv::Mat S = src_decentroid_coordinate * dst_decentroid_coordinate.t();
+        cv::Mat s = src_decentroid_coordinate * dst_decentroid_coordinate.t();
 
         cv::Mat u, w, vt;
-        cv::SVDecomp(S, w, u, vt);
+        cv::SVDecomp(s, w, u, vt);
 
-        R = vt.t()*u.t();     
+        cv::Mat mat_temp = u*vt;
+        double det = cv::determinant(mat_temp);
+        double datM[]={1,0,0,0,1,0,0,0,det};
+        cv::Mat matM(3,3,CV_64FC1, datM);
+
+        R = vt.t()*matM*u.t();     
         t = _dst_center - R*_src_center;
 
         for(int i = 0; i < 3; i++)
@@ -206,6 +235,7 @@ int main(int argc, char** argv)
         ros::NodeHandle nh;
 
         ros::Publisher chessboard_detected_pub = nh.advertise<std_msgs::Bool>("/vision/chessboard/had_detected", 10);
+        ros::Publisher  robot_pose_pub = nh.advertise<geometry_msgs::Pose>("/vision/robot_pose", 10);
 
         ros::Subscriber rgb_sub = nh.subscribe("/camera/color/image_raw", 10,  RGBCallBack);
         ros::Subscriber  depth_sub = nh.subscribe("/camera/aligned_depth_to_color/image_raw", 10, DepthCallBack);
@@ -216,35 +246,13 @@ int main(int argc, char** argv)
 
         ros::Rate rate(50);
 
-        // while(ros::ok())
-        // {
-        //         if(!depth.empty())
-        //         {
-        //                 for(int i = 0; i < depth.size().width; i++)
-        //                 {
-        //                         for(int j = 0; j < depth.size().height;j++)
-        //                         {
-        //                                 //if(depth.at<uint16_t >(j,i) > 1000) depth.at<uint16_t>(j,i) = 0;
-        //                                 cv::circle(depth, cv::Point2d(640,480), 2, cv::Scalar(255,0,0), 2);
-        //                                 depth.at<uint16_t>(j,i) = (uint16_t)(depth.at<uint16_t>(j,i) *32767./ 1000.);
-        //                         }
-        //                 }
-        //                 std::cout<<depth<<std::endl;
-        //                 cv::circle(depth, cv::Size(640,480),2,cv::Scalar(255,0,0),2);
-        //                 cv::imshow("depth", depth);
-                        
-        //         }
-        //         cv::waitKey(1);
-        //         ros::spinOnce();
-        // }
-
         std::vector<cv::Point3d> robot_points_list;
-        for(int i = 0; i < 9; i ++)
+        for(int i = 0; i < _CALIBRATE_POINT_NUM; i ++)
         {
                 cv::Point3d robot_point;
-                robot_point.x = (-0.1+0.1*(i/3))*m2mm;
-                robot_point.y = (0.1-0.1*(i%3))*m2mm;
-                robot_point.z = 0.35*m2mm;
+                robot_point.x = (-0.1+0.1*((i%9)/3))*m2mm;
+                robot_point.y = (0.3-0.1*(i%3))*m2mm;
+                robot_point.z = (0.25+0.05*(i/9))*m2mm;
 
                 robot_points_list.push_back(robot_point);
         }
@@ -281,43 +289,36 @@ int main(int argc, char** argv)
                                 ros::spinOnce();
                         }
                                 
-                                
-                        cv::Point3f center(0.,0.,0.), last_center(0., 0., 0.);
+                        cv::Point3f  real_center(0.,0.,0.), temp_center(0.,0.,0.);
                         int cnt = 0;
                         while(ros::ok())
                         {
-                                cnt ++;
-                                if(cnt >= 1000)break;
+                                cnt++;
+                                if(cnt >= 100)
+                                {
+                                        real_center /= cnt;
+                                        break;
+                                }
+                                
                                 if(!rgb.empty())
                                 {
-                                        cv::circle(rgb,cv::Point2f(320,240),2,cv::Scalar(0,0,255),2);
+                                        cv::circle(rgb, cv::Point2f(rgb.size().width/2, rgb.size().height/2),2,cv::Scalar(0,0,255),2);
                                         cv::circle(rgb,corners[corners.size()/2],2,cv::Scalar(255,0,0),2);
                                         cv::imshow("rgb", rgb);
+                                        //cv::imshow("depth", depth);
 
-
-                                                for(int i = 0; i < depth.size().width; i++)
-                                                {
-                                                        for(int j = 0; j < depth.size().height;j++)
-                                                        {
-                                                                //if(depth.at<uint16_t >(j,i) > 1000) depth.at<uint16_t>(j,i) = 0;
-                                                                cv::circle(depth,corners[corners.size()/2], 2, cv::Scalar(0,0,255), 10);
-                                                                depth.at<uint16_t>(j,i) = (uint16_t)(depth.at<uint16_t>(j,i) *32767./ 1000.);
-                                                        }
-                                                }
-
-                                        cv::imshow("depth", depth);
                                         cv::waitKey(10);
                                 }
 
-                                getCenter(corners, cameraMatrix, center);
-                                std::cout<<center.z<<std::endl;
-                                //if(abs(center.z - last_center.z)<10 && center.z > 500 && center.z < 5000 )break;
-                                        
-                                last_center = center;
+                                getCenter(corners, cameraMatrix, temp_center);
+
+                                real_center+=temp_center;
+
                                 ros::spinOnce();
                         }
-                        std::cout<< "Chessboard Detected! Center Coordinate: [ "<< center.x << ", " << center.y << ", " << center.z<<"]"<<std::endl;
-                        camera_points_list.push_back((cv::Point3d)center);
+
+                        ROS_INFO("Chessboard Detected! Center Coordinate: [ %lf, %lf, %lf]", real_center.x,real_center.y, real_center.z);
+                        camera_points_list.push_back((cv::Point3d)real_center);
                                 
                         std_msgs::Bool msg;
                         msg.data = true;
@@ -339,44 +340,29 @@ int main(int argc, char** argv)
         ICP(camera_points_list, robot_points_list, T, R, t);
         fromR2rpy(R, roll, pitch, yaw);
 
-        for(int i = 0 ; i<camera_points_list.size() ; i++)
-        {
-                std::cout<<camera_points_list[i].z<<std::endl;
-        }
+        ROS_INFO("x: [%lf], y: [%lf], z: [%lf], roll: [%lf], pitch: [%lf], yaw: [%lf]", t.at<double>(0,0), t.at<double>(1,0), t.at<double>(2,0), roll*rad2ang, pitch*rad2ang, yaw*rad2ang);
+        std::cout<<"Calculated T:"<<std::endl<<T<<std::endl;
 
-        std::cout<<"x: " << t.at<double>(0,0)<< "\ty: "<<t.at<double>(1,0)<<"\tz: "<<t.at<double>(2,0)<<"\troll: "<<roll * rad2ang<<"\tpitch:"<<pitch * rad2ang<<"\tyaw:"<<yaw*rad2ang<<std::endl;
-
-
-        // while(ros::ok())
+        // for(int i = 0; i <_CALIBRATE_POINT_NUM; i ++)
         // {
-        //         std::vector<cv::Point2f> corners;
-
-        //         getCornerList(rgb, corners);
+        //         cv::Mat homo(4,1,CV_64F);
                 
-        //         cv::Point3f center;
-        //         getCenter(corners, cameraMatrix, center);
-        //         if((last_center.x - center.x) < 10&&(last_center.y - center.y) < 10&&(last_center.z - center.z)<10
-        //                 &&center.z<3000&&center.z>100)
-        //         {
-        //                 //std::cout<<center<<std::endl;
-        //         }
+        //         homo.at<double>(0,0) = (double)camera_points_list[i].x;
+        //         homo.at<double>(1,0) = (double)camera_points_list[i].y;
+        //         homo.at<double>(2,0) = (double)camera_points_list[i].z;
+        //         homo.at<double>(3,0) = 1.;
 
-        //         last_center = center;
-
-        //         double roll, pitch, yaw;
-        //         cv::Mat R = cv::Mat::eye(cv::Size(3,3),CV_64F);
-        //         cv::Mat T;
-        //         getRT(corners, cameraMatrix, distCoeffs, R, T);
-        //         fromR2rpy(R, roll, pitch, yaw);
-
-        //         //std::cout<<"roll: "<<roll/M_PI*180<<"\tpitch:"<<pitch/M_PI*180<<"\tyaw:"<<yaw/M_PI*180<<std::endl;
-
-        //         if(!rgb.empty())
-        //                 cv::imshow("chessboard", rgb);
-
-        //         cv::waitKey(1);
-
-        //         rate.sleep();
-        //         ros::spinOnce();
+        //         std::cout<<"Calculate Transform ["<<i <<"]: "<<(T*homo)<<std::endl;
+        //         std::cout<<robot_points_list[i]<<std::endl;
         // }
+
+        geometry_msgs::Pose cam2robot;
+        cam2robot.position.x = t.at<double>(0,0);
+        cam2robot.position.y = t.at<double>(1,0);
+        cam2robot.position.z = t.at<double>(2,0);
+        fromrpy2Quaternion(roll, pitch, yaw, cam2robot);
+        robot_pose_pub.publish(cam2robot);
+
+        ros::spin();
+        return 0;
 }
